41348c54a14f883c4a23b4b4616feb47386c12a9
albi-00 <65985109+albi-00@users.noreply.github.com>
2020-08-31 14:31:41 -0300

    fjtgsirgiej
    fjvdn

--- infoProject/infoProject/settings/local.py
+++ infoProject/infoProject/settings/local.py
@@ -12,7 +12,7 @@ DATABASES = {
         'ENGINE': 'sql_server.pyodbc',
         'NAME': 'ProjectInfo',
         'Trusted_Connection':'yes',
-        'HOST':'localhost\SQLEXPRESS',
+        'HOST':'DESKTOP-VBORAD3\SQLEXPRESS',
         'OPTIONS':{
             'driver':'SQL Server Native Client 11.0'
         },
